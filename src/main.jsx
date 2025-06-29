223c7900ee981f8cc7027b75e933735eeea46ca3
nevin03 <nevinm2021@gmail.com>
2025-06-29 22:39:35 +0530

    refactor: remove unused token handling logic

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-
-import useAuthStore from "./store/useAuthStore";
 import { ToastProvider } from "./contexts/ToastProvider.jsx";
-
-const token = localStorage.getItem("token");
-if (token) {
-  useAuthStore.getState().login({ user: null, token });
-}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ToastProvider>
