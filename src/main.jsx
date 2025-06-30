db1a3b7780b620b7e7dca72d5f7a880b8d3fae88
nevin03 <nevinm2021@gmail.com>
2025-07-01 00:11:39 +0530

    chore: update main entry point with ToastProvider or routing changes

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { ToastProvider } from "./contexts/ToastProvider.jsx";
+import { ToastProvider } from "./contexts/ToastProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ToastProvider>
